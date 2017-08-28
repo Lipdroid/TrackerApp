037be5fb2d1ab1e6dece5af780e79e9a40d42478
Md Munir Hossain <lipuhossain67@gmail.com>
2017-08-28 17:02:16 +0600

    Upgraded

--- TrackerApp/MapViewController.swift
+++ TrackerApp/MapViewController.swift
@@ -19,6 +19,8 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //configure Google Signin
+        GIDSignIn.sharedInstance().clientID = FIRApp.defaultApp()?.options.clientID
     }
 
     override func didReceiveMemoryWarning() {
