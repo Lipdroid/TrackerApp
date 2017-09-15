b618e08e4e9e34826f03eaf992edec836aa51a5b
lipuhossain <kutu1234>
2017-09-15 22:34:56 +0600

    added users current location

--- TrackerApp/MapViewController.swift
+++ TrackerApp/MapViewController.swift
@@ -17,24 +17,20 @@ import SwiftKeychainWrapper
 
 class MapViewController: UIViewController {
     var mUserObj: UserObject! = nil
-
     @IBOutlet weak var google_map: GMSMapView!
-  
     @IBOutlet weak var user_image: CircleImageView!
     @IBOutlet weak var user_name_label: UILabel!
-    
+    let locationManager = CLLocationManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-        
-        
         initMaps()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
         if(mUserObj != nil){
             setUserData()
         }
-        
-        
     }
     
     private func setUserData(){
@@ -59,46 +55,29 @@ class MapViewController: UIViewController {
     private func initMaps(){
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: destination_latitude, longitude: destination_latitude, zoom: 15.0)
+//        let camera = GMSCameraPosition.camera(withLatitude: destination_latitude, longitude: destination_latitude, zoom: 5.0)
+//
+//        // Creates a marker in the center of the map.
+//        let marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2D(latitude: destination_latitude, longitude: destination_latitude)
+//        marker.title = "Oceanize"
+//        marker.snippet = "Bangladesh"
+//        marker.appearAnimation = .pop
+//        //marker.iconView = MarkerView()
+//       // marker.map = self.google_map
+//
+//        google_map.animate(to: camera)
+//        
+//        // Delay the dismissal by 5 seconds
+//        let when = DispatchTime.now() + 2 // change 2 to desired number of seconds
+//        DispatchQueue.main.asyncAfter(deadline: when) {
+//            // Your code with delay
+//            self.addMarker()
+//        }
 
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: destination_latitude, longitude: destination_latitude)
-        marker.title = "Oceanize"
-        marker.snippet = "Bangladesh"
-        marker.appearAnimation = .pop
-        //marker.iconView = MarkerView()
-       // marker.map = self.google_map
-
-        google_map.animate(to: camera)
-        
-        // Delay the dismissal by 5 seconds
-        let when = DispatchTime.now() + 2 // change 2 to desired number of seconds
-        DispatchQueue.main.asyncAfter(deadline: when) {
-            // Your code with delay
-            self.addMarker()
-        }
-
-
-    }
-    private func addMarker(){
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: destination_latitude, longitude: destination_latitude, zoom: 15.0)
-        
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: destination_latitude, longitude: destination_latitude)
-        marker.title = "Oceanize"
-        marker.snippet = "Bangladesh"
-        marker.appearAnimation = .pop
-        //marker.iconView = MarkerView()
-        marker.map = self.google_map
-        
-        google_map.animate(to: camera)
 
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -130,4 +109,43 @@ class MapViewController: UIViewController {
     }
     */
 
+}
+
+// MARK: - CLLocationManagerDelegate
+//An extension for showing user location
+extension MapViewController: CLLocationManagerDelegate {
+    private func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+            google_map.isMyLocationEnabled = true
+            google_map.settings.myLocationButton = true
+        }
+    }
+//locationManager(_:didUpdateLocations:) executes when the location manager receives new location data.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            google_map.camera = GMSCameraPosition(target: location.coordinate, zoom: 10, bearing: 0, viewingAngle: 0)
+            self.addMarker(location: location)
+            locationManager.stopUpdatingLocation()
+        }
+        
+    }
+    
+    private func addMarker(location: CLLocation){
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+//        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: 15.0)
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: location.coordinate.latitude ,longitude: location.coordinate.longitude)
+        marker.title = mUserObj.userName
+        marker.snippet = mUserObj.userEmail
+        marker.appearAnimation = .pop
+        //marker.iconView = MarkerView()
+        marker.map = self.google_map
+    
+       // google_map.animate(to: camera)
+        
+    }
+ 
 }
