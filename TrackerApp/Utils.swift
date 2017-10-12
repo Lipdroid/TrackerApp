5dafd3012eadbbfcd6fa32138e45d6f3c63b6766
Md Munir Hossain <lipuhossain67@gmail.com>
2017-10-12 16:21:57 +0600

    changed the marker design

--- TrackerApp/Utils.swift
+++ TrackerApp/Utils.swift
@@ -31,25 +31,25 @@ func createMarkerWithImage(url: String, completed: @escaping Completion){
         DispatchQueue.main.async(execute: { () -> Void in
             let image = UIImage(data: data!)
             ///Creating UIView for Custom Marker
-            let DynamicView=UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            let DynamicView=UIView(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
             DynamicView.backgroundColor=UIColor.clear
             
             //Creating Marker Pin imageview for Custom Marker
             var imageViewForPinMarker : UIImageView
-            imageViewForPinMarker  = UIImageView(frame:CGRect(x: 0, y: 0, width: 40, height: 50));
+            imageViewForPinMarker  = UIImageView(frame:CGRect(x: 0, y: 0, width: 60, height: 60));
             imageViewForPinMarker.image = UIImage(named:"marker_window")
             
             //Creating User Profile imageview
             var imageViewForUserProfile : UIImageView
-            imageViewForUserProfile  = UIImageView(frame:CGRect(x: 0, y: 0, width: 40, height: 35));
-            imageViewForUserProfile.layer.cornerRadius = 5.0
+            imageViewForUserProfile  = UIImageView(frame:CGRect(x: 0, y: 0, width: 40, height: 40));
+            imageViewForUserProfile.layer.cornerRadius = imageViewForUserProfile.frame.height/2
             imageViewForUserProfile.clipsToBounds = true
             imageViewForUserProfile.contentMode = .scaleAspectFill
             imageViewForUserProfile.image = image
             
             //set the profile pic in the center
-            //imageViewForUserProfile.center = CGPoint(x: imageViewForPinMarker.frame.size.width  / 2,
-            //     y: imageViewForPinMarker.frame.size.height / 2);
+            imageViewForUserProfile.center = CGPoint(x: imageViewForPinMarker.frame.size.width  / 2,
+                 y: imageViewForPinMarker.frame.size.height / 3 + 2);
             
             //Adding userprofile imageview inside Marker Pin Imageview
             imageViewForPinMarker.addSubview(imageViewForUserProfile)
